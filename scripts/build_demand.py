a423ff746c66508dc70f3c5fe6586050de7156ee
DeniseGiub <d.giubilato@studenti.unipi.it>
2022-12-02 12:23:59 +0000

    build_demand new part

---
+++ scripts/build_demand.py
@@ -0,0 +1,207 @@
+import os
+from _helpers import configure_logging, sets_path_to_root
+
+
+import geopandas as gpd
+import fiona
+import rasterio
+import rasterio.mask
+import georasters as gr
+import pandas as pd
+import geojson
+
+#%%
+# #This script downloads WorldPop data for SL country. 2019 data are taken, since 2020 data are not available for Sierra Leone
+
+# import requests 
+# import shutil
+# import os
+
+# def download_WorldPop_standard(
+#     country_code,
+#     year=2019,
+#     update=False,
+#     size_min=300
+# ):
+#     """
+#     Download tiff file for each country code using the standard method from worldpop datastore with 1kmx1km resolution.
+
+#     Parameters
+#     ----------
+#     country_code : str
+#         Two letter country codes of the downloaded files.
+        # Files downloaded from https://data.worldpop.org/ datasets WorldPop UN adjusted
+#     year : int
+#         Year of the data to download
+#     update : bool
+#         Update = true, forces re-download of files
+#     size_min : int
+#         Minimum size of each file to download
+#     Returns
+#     -------
+#     WorldPop_inputfile : str
+#         Path of the file
+#     WorldPop_filename : str
+#         Name of the file
+#     """
+#     WorldPop_filename = f"sle_ppp_{year}_constrained.tif"
+#     WorldPop_urls = [
+#             f"https://data.worldpop.org/GIS/Population/Global_2000_2020_Constrained/2019/BSGM/SLE/{WorldPop_filename}"]
+#     WorldPop_inputfile = os.path.join(
+#         os.getcwd(), "data", "WorldPop", WorldPop_filename
+#     )  # Input filepath tif
+
+#     if not os.path.exists(WorldPop_inputfile) or update is True:
+    
+#         #  create data/osm directory
+#         os.makedirs(os.path.dirname(WorldPop_inputfile), exist_ok=True)
+
+#         loaded = False
+#         for WorldPop_url in WorldPop_urls:
+#             with requests.get(WorldPop_url, stream=True) as r:
+#                 with open(WorldPop_inputfile, "wb") as f:
+#                     if float(r.headers["Content-length"]) > size_min:
+#                         shutil.copyfileobj(r.raw, f)
+#                         loaded = True
+#                         break
+
+#     return WorldPop_inputfile, WorldPop_filename
+
+
+# download_WorldPop_standard(
+#                     config["countries"], config["year"], False, 300)
+
+
+#****
+
+#The vertexes of the rectangular are defined: they are the point P1=(x1,y1), P2=(x2,y2), P3=(x3,y3), P4=(x4,y4)
+#x stands for longitude, y stands for latitude
+
+def create_microgrid_shape(xcenter, ycenter, DeltaX, DeltaY, name):
+
+    x1 = xcenter - DeltaX*0.5
+    y1 = ycenter + DeltaY*0.5
+
+    x2 = xcenter + DeltaX*0.5
+    y2 = ycenter + DeltaY*0.5
+
+    x3 = xcenter - DeltaX*0.5 
+    y3 = ycenter - DeltaY*0.5
+
+    x4 = xcenter + DeltaX*0.5
+    y4 = ycenter - DeltaY*0.5
+
+    microgrid_name=name
+    my_feature=[]
+
+    my_feature={
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates":  [
+            [
+              [x1,y1],
+              [x2,y2],
+              [x3,y3],         
+              [x4,y4],
+            ]
+        ]
+      },
+
+      "properties": {
+        "Microgrid": [microgrid_name]
+      }
+    }
+
+    return(my_feature)
+
+
+
+#I create a rectangle which will be the minigrid
+
+#%%
+# FeatureCollection = FeatureCollection([my_feature])
+
+# with open('microgrid_shape.geojson', 'w') as f:
+#         f.write(geojson.dumps(FeatureCollection))
+
+# gdf = gpd.read_file('microgrid_shape.geojson')
+# gdf.to_file('microgrid_shape.shp')
+
+# with fiona.open("microgrid_shape.shp", "r") as shapefile:
+#     shapes = [feature["geometry"] for feature in shapefile]
+
+# with rasterio.open('sle_ppp_2019_constrained.tif') as src:
+#     out_image, out_transform = rasterio.mask.mask(src, shapes, crop=True)
+#     out_meta = src.meta
+
+# out_meta.update({"driver": "GTiff",
+#                  "height": out_image.shape[1],
+#                  "width": out_image.shape[2],
+#                  "transform": out_transform})
+
+# with rasterio.open("SL.masked.tif", "w", **out_meta) as dest:
+#     dest.write(out_image)
+
+# myRaster = 'sle_ppp_2019_constrained.tif'
+# total_pop= gr.from_file(myRaster)
+    
+# total_pop=total_pop.to_geopandas() 
+
+# total_pop=(total_pop['value'].sum()) #Total SL population
+
+# myRaster = 'SL.masked.tif'
+# pop_microgrid = gr.from_file(myRaster)
+    
+# pop_microgrid=pop_microgrid.to_geopandas() 
+
+# pop_microgrid=(pop_microgrid['value'].sum()) #Microgrid population
+
+# #I import the dataframe of electricity demand for Africa
+# import pandas as pd
+# df_demand=pd.read_excel(r'C:\Users\denis\OneDrive\Desktop\Mini grids\pypsa-distribution\Africa.xlsx', index_col = None)
+
+# #I select the rows related to Benin (since there are no data for SL)
+# df_demand_SL=df_demand.loc[26280:35039, :]
+
+# # I select the column "electricity demand"
+# df_demand_SL=df_demand_SL["Electricity demand"]
+
+# df_demand_SL=pd.DataFrame(df_demand_SL)
+
+# p=(pop_microgrid/total_pop)*100 #Coefficient
+
+# electric_load=df_demand_SL/p #Electric load of the minigrid
+
+# electric_load_xlsx=electric_load.to_excel('electric_load.xlsx', index=False) 
+
+def writeToGeojsonFile(path, fileName, data):
+    filePathNameWExt = './' + path + '/' + fileName + '.geojson'
+    with open(filePathNameWExt, 'w') as fp:
+        geojson.dump(data, fp)
+
+
+
+# './' represents the current directory so the directory save-file.py is in
+# 'test' is my file name
+if __name__ == "__main__":
+    if "snakemake" not in globals():
+        from _helpers import mock_snakemake
+
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+        snakemake = mock_snakemake("build_demand")
+
+    configure_logging(snakemake)
+
+    
+    my_feature=create_microgrid_shape(
+        snakemake.config["microgrids_list"]["Location"]["Centre"]["lon"],
+        snakemake.config["microgrids_list"]["Location"]["Centre"]["lat"],
+        snakemake.config["microgrids_list"]["Location"]["Sides"]["Deltalon"],
+        snakemake.config["microgrids_list"]["Location"]["Sides"]["Deltalat"],
+        snakemake.config["microgrids_list"]["micA"]["name"],
+    )
+
+    writeToGeojsonFile('./','microgrid_shape', my_feature)
+
+
