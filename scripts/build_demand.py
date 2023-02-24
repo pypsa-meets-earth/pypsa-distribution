6d30cc5edcaaadb60d4ad009273afc7d4d3a365f
DeniseGiub <d.giubilato@studenti.unipi.it>
2023-02-24 09:51:09 +0100

    For loop for multiple microgrids

--- scripts/build_demand.py
+++ scripts/build_demand.py
@@ -27,7 +27,7 @@ Inputs
 Outputs
 -------
 - ``data/Worldpop/population_file.tif: a tif file of the population of the selected country,
-- ``resources/shapes/microgrid_shape.geojson: a geojson file of the shape of the microgrid,
+- ``resources/shapes/microgrid_shapes.geojson: a geojson file of the shape of the microgrid,
 - ``resources/file_dir/country_masked.tif,
 - ``resources/demand/microgrid_load.csv: the electric load of the microgid,
 -------
@@ -51,147 +51,57 @@ import pandas as pd
 import rasterio
 import rasterio.mask
 import requests
+from shapely.geometry import Polygon
 from _helpers import configure_logging, get_country, sets_path_to_root
-
+import json
 _logger = logging.getLogger(__name__)
 _logger.setLevel(logging.INFO)
 
 
-def create_microgrid_shape(xcenter, ycenter, DeltaX, DeltaY, name, output_path):
+def create_microgrid_shapes(microgrids_list, output_path): 
+
     """
-    This function creates a rectangular shape and saves it as a .geojson file.
-    The shape is defined by its center coordinates (xcenter, ycenter) and its dimensions (DeltaX, DeltaY).
-    The shape is also given a name, which is saved as a property in the .geojson file.
+    This function creates a rectangular shape of the microgrid and saves it as a .geojson file.
+    The shape is defined by the coordinates of the angles of the rectangle.
     The resulting file is saved to the specified output_path.
     """
+    
+    microgrids_list=microgrids_list
+    microgrids_list_df=pd.DataFrame(microgrids_list)
 
-    # Define the coordinates of the corners of the rectangle
-    x1 = xcenter - DeltaX * 0.5
-    y1 = ycenter + DeltaY * 0.5
+    microgrid_shapes = []
+    microgrid_names = []
 
-    x2 = xcenter + DeltaX * 0.5
-    y2 = ycenter + DeltaY * 0.5
+    for col in range(len(microgrids_list_df.columns)):
 
-    x3 = xcenter - DeltaX * 0.5
-    y3 = ycenter - DeltaY * 0.5
+        values=microgrids_list_df.iloc[:, col]
 
-    x4 = xcenter + DeltaX * 0.5
-    y4 = ycenter - DeltaY * 0.5
+        x1 = values[1] #lon_max
+        y1 = values[3] #lat_max
 
-    microgrid_name = name
+        x2 = values[0] #lon_min
+        y2 = values[3] #lat_max
 
-    my_feature = {
-        "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "properties": {"name": "microgrid_name"},
-                "geometry": {
-                    "type": "Polygon",
-                    "coordinates": [[[x1, y1], [x2, y2], [x3, y3], [x4, y4]]],
-                },
-            },
-        ],
-    }
+        x3 = values[0] #lon_min
+        y3 = values[2] #lat_min 
 
-    # Convert the feature to a GeoDataFrame
-    gdf = gpd.GeoDataFrame.from_features(my_feature)
-
-    # Save the GeoDataFrame to a .geojson file
-    gdf.to_file(output_path)
+        x4 = values[1] #lon_max
+        y4 = values[2] #lat_min
 
 
-def download_WorldPop_standard(
-    output_path,
-    country_code,
-    year,
-    update,
-    out_logging,
-    size_min,
-):
-    """
-    Download tiff file for each country code using the standard method from worldpop datastore with 1kmx1km resolution.
+        microgrid_shape = Polygon([(x1, y1), (x2, y2), (x3, y3), (x4, y4), (x1, y1)])
 
-    Parameters
-    ----------
-    country_code : str
-        Two letter country codes of the downloaded files.
-        Files downloaded from https://data.worldpop.org/ datasets WorldPop UN adjusted
-    year : int
-        Year of the data to download
-    update : bool
-        Update = true, forces re-download of files
-    size_min : int
-        Minimum size of each file to download
-    Returns
-    -------
-    WorldPop_inputfile : str
-        Path of the file
-    WorldPop_filename : str
-        Name of the file
-    """
+        microgrid_name=f"microgrid_{col+1}"
+        microgrid_shapes.append(microgrid_shape)
+        microgrid_names.append(microgrid_name)
 
-    if out_logging:
-        _logger.info("Download WorldPop datasets")
+    microgrid_gdf = gpd.GeoDataFrame({'name': microgrid_names, 'geometry': microgrid_shapes})
 
-    three_digits_code = get_country("alpha_3", alpha_2="SL")
+    output_dict = json.loads(microgrid_gdf.to_json())
+    output_json = json.dumps(output_dict, indent=4)
 
-    def convert_to_lowercase(string):
-        return string.lower()
-
-    three_digits_code_lower = convert_to_lowercase(three_digits_code)
-
-    WorldPop_filename = f"{three_digits_code_lower}_ppp_{year}_constrained.tif"
-
-    if {year} == "2019":
-        WorldPop_urls = [
-            f"https://data.worldpop.org/GIS/Population/Global_2000_2020_Constrained/{year}/{three_digits_code}/{WorldPop_filename}"
-        ]
-
-    # if {year} == "2020":
-
-    #     WorldPop_urls = [
-    #            f"https://data.worldpop.org/GIS/Population/Global_2000_2020_Constrained/{year}/BSGM/{WorldPop_filename}"
-    #     ]
-
-    WorldPop_inputfile = os.path.join(
-        os.getcwd(),
-        "data",
-        "WorldPop",
-        f"{three_digits_code_lower}_ppp_{year}_constrained.tif",
-    )  # Input filepath tif
-
-    old_file_paths = glob.glob(os.path.join("data/Worldpop", "*_ppp_*_constrained.tif"))
-
-    for old_file_path in old_file_paths:
-        shutil.move(old_file_path, output_path)
-
-    # os.remove(WorldPop_inputfile)
-
-    if not os.path.exists(WorldPop_inputfile) or update is True:
-        if out_logging:
-            _logger.warning(
-                f" {WorldPop_filename} does not exist, downloading to {WorldPop_inputfile}"
-            )
-        #  create data/osm directory
-        os.makedirs(os.path.dirname(WorldPop_inputfile), exist_ok=True)
-
-        loaded = False
-
-        WorldPop_urls = [
-            f"https://data.worldpop.org/GIS/Population/Global_2000_2020_Constrained/{year}/{three_digits_code}/{WorldPop_filename}",
-        ]
-        # f"https://data.worldpop.org/GIS/Population/Global_2000_2020_Constrained/{year}/{three_digits_code}/{WorldPop_filename}"
-
-        for WorldPop_url in WorldPop_urls:
-            with requests.get(WorldPop_url, stream=True) as r:
-                with open(WorldPop_inputfile, "wb") as f:
-                    if float(r.headers["Content-length"]) > size_min:
-                        shutil.copyfileobj(r.raw, f)
-                        loaded = True
-                        break
-        if not loaded:
-            _logger.error(f" Impossible to download {WorldPop_filename}")
+    with open(output_path, "w") as f:
+        f.write(output_json)
 
 
 def create_masked_file(raster_path, geojson_path, output_path):
@@ -203,10 +113,10 @@ def create_masked_file(raster_path, geojson_path, output_path):
 
     # Read the geojson file and convert it to a shapefile
     gdf = gpd.read_file(geojson_path)
-    gdf.to_file("microgrid_shape.shp")
+    gdf.to_file("microgrid_shapes.shp")
 
     # Open the shapefile and extract the shape geometry
-    with fiona.open("microgrid_shape.shp", "r") as shapefile:
+    with fiona.open("microgrid_shapes.shp", "r") as shapefile:
         shapes = [feature["geometry"] for feature in shapefile]
 
     # Open the raster and mask it using the shapes
@@ -239,7 +149,7 @@ def estimate_microgrid_population(masked_file, p, sample_profile, output_file):
     with rasterio.open(masked_file) as fp:
         data = fp.read(1)
         pop_microgrid = data[data >= 0].sum()
-
+        
     # Read the sample profile of electricity demand
     total_load = pd.read_csv(sample_profile)
     total_load = total_load["0"]
@@ -266,30 +176,16 @@ if __name__ == "__main__":
         sets_path_to_root("pypsa-distribution")
 
     configure_logging(snakemake)
-
+    
     sample_profile = snakemake.input["sample_profile"]
 
-    create_microgrid_shape(
-        snakemake.config["microgrids_list"]["Location"]["Centre"]["lon"],
-        snakemake.config["microgrids_list"]["Location"]["Centre"]["lat"],
-        snakemake.config["microgrids_list"]["Location"]["Sides"]["Deltalon"],
-        snakemake.config["microgrids_list"]["Location"]["Sides"]["Deltalat"],
-        snakemake.config["microgrids_list"]["micA"]["name"],
-        snakemake.output["microgrid_shape"],
-    )
-
-    download_WorldPop_standard(
-        snakemake.output["Worldpop_data"],
-        snakemake.config["countries"],
-        snakemake.config["year"],
-        False,
-        False,
-        300,
-    )
+    create_microgrid_shapes(
+        snakemake.config["microgrids_list"], 
+        snakemake.output["microgrid_shapes"],)
 
     create_masked_file(
         "data/Worldpop/population_file.tif",
-        snakemake.output["microgrid_shape"],
+        snakemake.output["microgrid_shapes"],
         snakemake.output["country_masked"],
     )
 
